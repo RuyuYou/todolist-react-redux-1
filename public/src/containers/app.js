91d4aac50a79cca610a5b952f2c447985978ecf0
chengyurou <18829292695@163.com>
2016-08-22 00:08:56 -0400

    project init

---
+++ public/src/containers/app.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {render} from 'react-dom'
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                AddTodo
+            </div>
+        )
+    }
+}
+
+export default App;

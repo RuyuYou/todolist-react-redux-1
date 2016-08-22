06b66ec1caa37e84d6cc45e28ab718c4486897e9
chengyurou <18829292695@163.com>
2016-08-22 03:39:21 -0400

    can add task

--- public/src/containers/app.js
+++ public/src/containers/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import {render} from 'react-dom'
 import TodoList from './todo-list'
+import AddTodo from './add-todo'
 
 class App extends Component{
     render() {
         return (
             <div>
+                <AddTodo/>
                 <TodoList/>
             </div>
         )
--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,5 +1,14 @@
-export default function (state={ todos:
-    [{task:"cheng",id:0,isDone:false}]},action) {
+let id=0;
+export default function (state={ todos: []},action) {
+
+    switch(action.type){
+        case 'ADD':
+            state.todos.push({task:action.task,id:id++,isDone:false});
+            console.log(state.todos);
+            return {
+                todos:[...state.todos]
+            };
+    }
 
     return state;
 }
